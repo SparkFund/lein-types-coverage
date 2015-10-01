f35c87f648f55ed282b7e558bd3c83969059fc11
Nathan Sorenson <nathan.d.sorenson@gmail.com>
2015-09-30 23:18:19 -0700

    update project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-types-coverage "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Ensures all functions with Typed Clojure annotations are dynamically enforced by types-to-schema wrappers during lein test."
+  :url "https://github.com/SparkFund/lein-types-coverage"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :eval-in-leiningen true)
